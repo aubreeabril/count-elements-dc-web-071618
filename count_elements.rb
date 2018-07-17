dcc8a54d514455a251f03ba7ad3bb591f373f5de
aubreeabril <aubree.abril@gmail.com>
2018-07-17 15:15:01 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
+
+
 def count_elements(array)
-  # code goes here
+# make an empty hash
+  new_hash = {}
+# loop through the array
+  array.each do |item|
+    # set the keys and set the values to the number of times that item appears.
+    new_hash[item] = array.count(item)
+  end
+  new_hash
 end
- 
